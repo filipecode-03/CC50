f4f7ae93941b93402e677b881f62da179d5b1799
filipecode-03 <158044004+filipecode-03@users.noreply.github.com>
2025-01-30 10:40:26 -0400

    mudei o nome da pasta

---
+++ M6 - Python 🐍/3 - Input & condições/ex02.py
@@ -0,0 +1,10 @@
+from cs50 import get_int
+
+# Solicitar x ao usuário
+x = get_int("x: ")
+
+# Solicitar ao usuário y
+y = get_int("y: ")
+
+# Executar adição
+print(x + y)
---
+++ M6 - Python 🐍/3 - Input & condições/ex03.py
@@ -0,0 +1,8 @@
+# Solicitar x ao usuário
+x = input("x:")
+
+# Solicitar ao usuário y
+y = input("y:")
+
+# Executar adição
+print(x + y)
